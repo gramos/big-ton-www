14fbb95dbe5556a69c59c9bc99fbdfd388e3ee51
Gaston Ramos <ramos.gaston@gmail.com>
2017-03-03 08:49:01 -0300

    First real commit.

---
+++ app.rb
@@ -0,0 +1,22 @@
+require 'cuba'
+require 'mote'
+require 'mote/render'
+
+Cuba.plugin Mote::Render
+
+Cuba.use Rack::Static,
+         urls: %w[/js /css /img],
+         root: File.expand_path("./public", __dir__)
+
+# ----------------------------------------------------------------
+# Main app.
+#
+Cuba.define do
+
+  on root do
+    songs = %w(medios-de-comunicacion ahora-tengo-el-flow letme-back
+               al-fin-es-viernes sigue-al-rastaman paren-de-fumigar
+               resistencia-contra-babilon solo-en-la-ciudad)
+    render 'index', {:songs => songs}
+  end
+end
